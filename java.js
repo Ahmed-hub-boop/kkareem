2a4ee5f23ac13d0d0a0ad4a15de789b0d7a2795f
Ahmed-hub-boop <60401523+Ahmed-hub-boop@users.noreply.github.com>
2023-07-04 21:47:56 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+function omenu(){
+    document.getElementById('r-m').classList.add('open');
+    document.getElementById('m').classList.add('open');
+    document.getElementById('x').classList.add('open');
+}
+function cmenu(){
+    document.getElementById('r-m').classList.remove('open');
+    document.getElementById('m').classList.remove('open');
+    document.getElementById('x').classList.remove('open');
+}
